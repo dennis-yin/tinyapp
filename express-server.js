c8e45b855dc87f16394c49b38dc1484a045fde44
dennis-yin <dennis.yin26@gmail.com>
2019-10-31 21:36:41 +0000

    meeting some behaviour requirements

--- express-server.js
+++ express-server.js
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 
 // Routes
 app.get("/", (req, res) => {
-  if (users[req.session["user_id"]]) {
+  if (users[req.session.user_id]) {
     res.redirect("/urls");
   } else {
     res.redirect("/login");
@@ -35,19 +35,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  if (!users[req.session["user_id"]]) {
+  if (!users[req.session.user_id]) {
     res.send("Please login first");
   }
-  const temp = helpers.urlsForUser(req.session["user_id"], urlDatabase);
+  const temp = helpers.urlsForUser(req.session.user_id, urlDatabase);
   let templateVars = { urls: temp };
-  if (users[req.session["user_id"]]) {
-    templateVars.user = users[req.session["user_id"]];
+  if (users[req.session.user_id]) {
+    templateVars.user = users[req.session.user_id];
   }
   res.render("urls-index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  if (users[req.session["user_id"]]) {
+  if (users[req.session.user_id]) {
     let templateVars = { user: users[req.session.user_id] }
     res.render("urls-new", templateVars);
   } else {
@@ -56,40 +56,50 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
-  res.redirect("/urls");
+  if (users[req.session.user_id]) {
+    res.redirect("/urls");
+  } else {
+    res.render("register");
+  }
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
-  res.redirect("/urls");
+  if (users[req.session.user_id]) {
+    res.redirect("/urls");
+  } else {
+    res.render("login");
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const urls = helpers.urlsForUser(req.session["user_id"], urlDatabase);
+  const urls = helpers.urlsForUser(req.session.user_id, urlDatabase);
 
   if (!(Object.keys(urls).includes(shortURL))) {
     res.status(300).send("This URL does not belong to you");
   }
 
   let templateVars = { longURL: urlDatabase[shortURL].longURL, shortURL: shortURL };
-  if (users[req.session["user_id"]]) {
-    templateVars.user = users[req.session["user_id"]];
+  if (users[req.session.user_id]) {
+    templateVars.user = users[req.session.user_id];
   }
   res.render("urls-show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(longURL);
+  const shortURL = urlDatabase[req.params.shortURL];
+  if (shortURL) {
+    res.redirect(shortURL.longURL);
+  } else {
+    res.status(300).send("This URL does not exist");
+  }
 });
 
 app.post("/register", (req, res) => {
   const randomID = helpers.generateRandomString();
   const user = req.body;
 
-  if (!user.email || !user.password) {
+  if (!(user.email || user.password)) {
     res.status(400).send("400 error: Invalid email or password");
   }
 
@@ -105,23 +115,29 @@ app.post("/register", (req, res) => {
 
 app.post("/urls", (req, res) => {
   const randomURL = helpers.generateRandomString();
-  urlDatabase[randomURL] = { longURL: req.body["longURL"], userID: req.session["user_id"] };
+  urlDatabase[randomURL] = { longURL: req.body["longURL"], userID: req.session.user_id };
   res.redirect(`/urls/${randomURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
-    delete urlDatabase[req.params.shortURL];
-    res.redirect("/urls");
+  if (req.session.user_id) {
+    if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
+      delete urlDatabase[req.params.shortURL];
+      res.redirect("/urls");
+    } else {
+      res.status(300).send("That URL does not belong to you");
+    }
+  } else {
+    res.status(300).send("Please login before deleting URLs");
   }
 });
 
 // Edit the long URL that relates to a short URL
 app.post("/urls/:shortURL", (req, res) => {
-  if (users[req.session["user_id"]] === urlDatabase[req.params.shortURL].userID) {
+  if (users[req.session.user_id] === urlDatabase[req.params.shortURL].userID) {
     const newURL = req.body.newURL;
     const shortURL = req.params.shortURL;
-    const userID = req.session["user_id"];
+    const userID = req.session.user_id;
     urlDatabase[shortURL] = { newURL, userID };
     res.redirect("/urls");
   }
